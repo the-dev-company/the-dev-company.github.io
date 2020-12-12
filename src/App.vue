3f6e272bca9a28e1b0b44cba76a0dc8ddfbe71a8
Max Netterber <max.netterberg@gmail.com>
2020-12-12 18:55:50 +0100

    added tailwind and rewrote css

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,24 @@
 <template>
   <Navbar />
-  <div class="wrapper">
-    <p class="subtitle">THE DEVELOPER COMPANY</p>
-    <p class="ingress">
+  <div class="flex flex-col justify-center items-center text-center ">
+    <p class="text-white text-2xl sm:text-4xl mt-8 font-bold">
+      THE DEVELOPER COMPANY
+    </p>
+    <p class="text-justify text-white sm:text-3xl my-12 w-10/12 sm:max-w-2xl">
       Software designed for possibilities. DevComp is a consultancy firm based
       in Uppsala, Sweden. We offer digital solutions tailor made to your needs
       and liking. Contact us if you need to further develop your business with a
       custom software solution.
     </p>
-    <div class="contact-container">
-      <p class="contact" style="margin-bottom: 10px">Contact us at:</p>
-      <a class="contact" href="mailto:info@devcompany.se">info@devcompany.se</a>
-    </div>
-    <p
-      class="ingress"
-      style="text-align: center; margin-top: 30px; font-size: 15px"
+    <div
+      class="text-white text-2xl sm:text-3xl flex flex-col justify-center items-center font-bold"
     >
+      <p class="mb-5">Contact us at:</p>
+      <a class="underline" href="mailto:info@devcompany.se"
+        >info@devcompany.se</a
+      >
+    </div>
+    <p class=" text-white text-sm m-5">
       This page is currently a work in progress, drop by in the future to see
       what we're up to! 🚀
     </p>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: ["./src/**/*.vue"],
+  theme: {
+    fontFamily: {
+      main: ["Glacial Indifference", "sans-serif"],
+      sans: ["sans-serif"],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
