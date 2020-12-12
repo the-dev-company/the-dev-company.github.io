c4fe7d67ef0adf97531ab7ababe3505858508fe5
Max Netterber <max.netterberg@gmail.com>
2020-12-12 18:13:30 +0100

    added custom fonts

--- src/Navbar.vue
+++ src/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav style="width: 100%; height: auto; font-weight: bold">
+  <nav style="">
     <p class="title-text" style="margin: 0px 20px 0 20px">DevComp</p>
   </nav>
 </template>
@@ -9,3 +9,12 @@ import { defineComponent } from "vue";
 
 export default defineComponent({});
 </script>
+
+<style scoped>
+nav {
+  width: 100%;
+  height: auto;
+  font-weight: bold;
+  font-family: sans-serif;
+}
+</style>
